7585c73b7a70eb50644833918b2a3133afa2c2b9
mas0061 <mas0061@gmail.com>
2015-11-14 06:35:28 +0900

    Gulpからのリリースは、AzureにしたのでTODOコメント削除

--- gulpfile.js
+++ gulpfile.js
@@ -51,8 +51,4 @@ gulp.task('release', ['browserify'], function() {
   .pipe(gulp.dest('public'))
 });
 
-gulp.task('deploy', ['release'], function() {
-  // @todo mas0061.netへのリリース
-});
-
 gulp.task('default', ['browserify', 'watch', 'browserSync']);
