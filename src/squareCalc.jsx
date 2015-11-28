7316bd6eaa77eb260a4b358155cbce3b5f885efc
mas0061 <mas0061@gmail.com>
2015-11-29 07:13:01 +0900

    iPadでヘッダーが出なくなる現象を修正

--- src/squareCalc.jsx
+++ src/squareCalc.jsx
@@ -19,7 +19,7 @@ export default class SquareCalc extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <SCHeader handleEvent={this._handleEvent.bind(this)} />
         <SCMain squareNum={5} event={this.state.event} />
       </div>
