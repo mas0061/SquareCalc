800ce01fc6e73715d96aa0910a5d70085824f915
mas0061 <mas0061@gmail.com>
2016-04-09 06:55:51 +0900

    MDLの都合で階層変更

--- src/squareCalc.jsx
+++ src/squareCalc.jsx
@@ -19,9 +19,11 @@ export default class SquareCalc extends React.Component {
 
   render() {
     return (
-      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <SCHeader handleEvent={this._handleEvent.bind(this)} />
-        <SCMain squareNum={5} event={this.state.event} />
+      <div>
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+          <SCHeader handleEvent={this._handleEvent.bind(this)} />
+          <SCMain squareNum={5} event={this.state.event} />
+        </div>
       </div>
     );
   }
